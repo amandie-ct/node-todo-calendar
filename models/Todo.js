80599c7347b71021ce4991d14f0007a4848025ed
Amanda Carvalho <amandatcarvalho@hotmail.com.br>
2022-05-11 10:41:28 -0300

    saving progress

---
+++ models/Todo.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const todoSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    time: {
+        type: Date,
+        required: true
+    }
+},
+{
+    timestamps: true
+})
+
+export default mongoose.model('Todo', todoSchema);
